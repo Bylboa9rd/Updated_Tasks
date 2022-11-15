65a5900d3b0db56845b15684261d8da2996090bb
Bylboa9rd <110336767+Bylboa9rd@users.noreply.github.com>
2022-11-15 10:10:40 +0100

    Add files via upload

---
+++ LeapYear/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace LeapYear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int count = 0;
+            int currentLeapYear = 2020;
+            Console.WriteLine($"The current leap year is {currentLeapYear}");
+
+            while (count < 20)
+            {
+                Console.WriteLine($"The next leap year is {currentLeapYear += 4}");
+                count++;    
+            }
+        }
+    }
+}
